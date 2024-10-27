83081517a9ac1eb083cf36e7870f64eda1ba602b
William Liu <wl687@cornell.edu>
2024-10-26 21:51:49 -0400

    Implement minimal GUI interface in main.ml
    - added starter code that is available on the Bogue public API for an executable GUI interface
    - added bogue to the libraries stanza in bin/dune

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open Bogue
+module W = Widget
+module L = Layout
+
+let main () =
+  let b = W.check_box () in
+  let l = W.label "Hello world" in
+  let layout = L.flat_of_w [ b; l ] in
+
+  let board = Bogue.of_layout layout in
+  Bogue.run board
+
+let () =
+  main ();
+  Bogue.quit ()
