4b7d9e179ea089a3b17b0041cc980c94829a7161
William Liu <wl687@cornell.edu>
2024-11-05 23:21:08 -0500

    Implement ranged attack mechanic in main.ml
    - implemented a "shoot" mechanic that is just a ranged projectile that is launched by the player
    - the projectile is fired using the spacebar and always originates from the player's position
    - defined a projectile type that may (and probably will) become its own separate compilation unit in the future

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,45 @@
 open Graphics
 open Dungeon_crawler.Player
 
+type projectile = {
+  x : int;
+  y : int;
+  speed : int;
+}
+
 let player1 = create_player 40 40 50 50
+let projectiles = ref []
 
 let draw_player player =
   draw_rect (current_x_pos player) (current_y_pos player) (get_height player)
     (get_width player)
 
+let draw_projectiles () =
+  List.iter (fun p -> draw_rect p.x p.y 5 5) !projectiles
+
+let move_projectiles () =
+  projectiles := List.map (fun p -> { p with x = p.x + p.speed }) !projectiles;
+  projectiles := List.filter (fun p -> p.x < size_x ()) !projectiles
+
+let shoot player =
+  let new_projectile =
+    {
+      x = current_x_pos player + (get_width player / 2);
+      y = current_y_pos player + (get_height player / 2);
+      speed = 1;
+    }
+  in
+  projectiles := new_projectile :: !projectiles
+
 let update_player player =
-  match read_key () with
-  | 'w' -> move_player player 0 2
-  | 's' -> move_player player 0 (-2)
-  | 'a' -> move_player player (-2) 0
-  | 'd' -> move_player player 2 0
-  | _ -> ()
+  if key_pressed () then
+    match read_key () with
+    | 'w' -> move_player player 0 2
+    | 's' -> move_player player 0 (-2)
+    | 'a' -> move_player player (-2) 0
+    | 'd' -> move_player player 2 0
+    | ' ' -> shoot player (* ' ' indicates spacebar *)
+    | _ -> ()
 
 (* let get_corners player = {}
 
@@ -22,11 +48,14 @@ let update_player player =
    corners_obj2 = get_corners obj2 in if corners_obj1 *)
 
 let () =
+  open_graph "";
   while true do
     auto_synchronize false;
-    open_graph "";
+    clear_graph ();
     draw_rect 4 5 300 200;
     draw_player player1;
+    draw_projectiles ();
     update_player player1;
+    move_projectiles ();
     synchronize ()
   done
